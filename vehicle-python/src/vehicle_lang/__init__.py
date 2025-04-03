63c0f07e560354c5cf03ea0184e692cb783d30a8
Ann Roy <97691656+developing-ar@users.noreply.github.com>
2025-04-03 16:06:29 +0800

    877: `check`, `validate` & `export` python bindings

--- vehicle-python/src/vehicle_lang/__init__.py
+++ vehicle-python/src/vehicle_lang/__init__.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from ._version import VERSION as VERSION
+from .compile import compile_to_query as compile_to_query
 from .compile.error import VehicleBuiltinUnsupported as VehicleBuiltinUnsupported
 from .compile.error import VehiclePropertyNotFound as VehiclePropertyNotFound
 
@@ -13,20 +14,25 @@ from .typing import AnyOptimiser as AnyOptimiser
 from .typing import AnyOptimisers as AnyOptimisers
 from .typing import DeclarationName as DeclarationName
 from .typing import DifferentiableLogic as DifferentiableLogic
-from .typing import QueryFormat as QueryFormat
 from .typing import Optimiser as Optimiser
 from .typing import QuantifiedVariableName as QuantifiedVariableName
+from .typing import QueryFormat as QueryFormat
 from .typing import Verifier as Verifier
 from .verify import verify as verify
-from .compile import compile_to_query as compile_to_query
 
 __all__: List[str] = [
     "VERSION",
+    # Check
+    "check",
     # Compile
     "load_loss_function",
-	"compile_to_query",
+    "compile_to_query",
     # Verify
     "verify",
+    # Validate,
+    "validate",
+    # export
+    "export_to_solver",
     # Error types
     "VehicleError",
     "VehicleSessionClosed",
@@ -41,6 +47,6 @@ __all__: List[str] = [
     "AnyOptimiser",
     "AnyOptimisers",
     "DifferentiableLogic",
-	"QueryFormat",
+    "QueryFormat",
     "Verifier",
 ]
